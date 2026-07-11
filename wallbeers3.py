25b7676db567a10860efbe8937038ddb63a5a6af
Charles Russella <111414629+crussella0129@users.noreply.github.com>
2026-07-11 10:35:31 -0400

    Implement '99 Bottles of Beer' song logic

--- wallbeers3.py
+++ wallbeers3.py
@@ -1,4 +1,3 @@
-a=99
 b=" bottle"
 c=f"{b}s of beer" 
 d=f"{c} on the wall"
@@ -7,7 +6,7 @@ f=f"{b} of beer"
 g=f"{f} on the wall"
 h="o more"
 i="Go to the store and buy some more, "
-for j in range(a, -1, -1):
+for j in range(99, -1, -1):
    if j > 2:
       print(f"{j}{d}, {j}{c}.\n{e}{j-1}{d}.\n")
    elif j == 2:
@@ -15,4 +14,4 @@ for j in range(a, -1, -1):
    elif j == 1:
       print(f"{j}{g}, {j}{f}.\n{e}n{h}{d}.\n")
    else:
-      print(f"N{h}{d}, n{h}{c}.\n{i}{a}{d}.")
+      print(f"N{h}{d}, n{h}{c}.\n{i}99{d}.")
